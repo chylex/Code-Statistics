c8609d0259e98969666377187e55bdc283115caa
chylex <info@chylex.com>
2015-09-08 16:10:37 +0200

    Add basic file handler and tab classes

---
+++ CodeStatistics/Handlers/FileHandler.cs
@@ -0,0 +1,29 @@
+﻿using CodeStatistics.ConsoleUtil;
+using CodeStatistics.Input;
+
+namespace CodeStatistics.Handlers{
+    abstract class FileHandler{
+        public abstract class Minor : FileHandler{
+            public Minor(){}
+        }
+
+        public abstract class Major : FileHandler{
+            public Major(){}
+
+            public abstract string GetName();
+        }
+
+        public abstract void Handle(File file);
+
+        public abstract IHandlerTab[] GenerateTabs();
+
+        /// <summary>
+        /// Can further determine whether the file is valid by analyzing the contents.
+        /// </summary>
+        public bool IsFileValid(File file){
+            return true;
+        }
+
+        private FileHandler(){}
+    }
+}
---
+++ CodeStatistics/Handlers/IHandlerTab.cs
@@ -0,0 +1,8 @@
+﻿using CodeStatistics.ConsoleUtil;
+
+namespace CodeStatistics.Handlers{
+    interface IHandlerTab{
+        string GetName();
+        void RenderInfo(ConsoleWrapper c, int y);
+    }
+}
