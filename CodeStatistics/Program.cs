77c3c3f0b69a6ae2af0e44b01538b44c9cd3bd02
chylex <info@chylex.com>
2015-09-08 02:11:12 +0200

    Setup the console with initial project selection dialog

--- CodeStatistics/Program.cs
+++ CodeStatistics/Program.cs
@@ -1,4 +1,5 @@
-﻿using CodeStatistics.Input;
+﻿using CodeStatistics.ConsoleUtil;
+using CodeStatistics.Input;
 using System;
 
 namespace CodeStatistics{
@@ -7,7 +8,30 @@ namespace CodeStatistics{
 
         [STAThread]
         static void Main(string[] args){
+            int width = Math.Min(ConsoleWidth,Console.LargestWindowWidth);
+            int height = Math.Min(ConsoleHeight,Console.LargestWindowHeight);
+            Console.SetWindowSize(width,height);
+            Console.SetBufferSize(width,height);
+
+            ConsoleWrapper console = ConsoleWrapper.console;
+
+            console.SetForeground(ConsoleColor.White);
+            console.WriteCenter(1,"Choose your project selection method:");
+
+            ConsoleTabs<IProjectInputMethod> inputTabs = new ConsoleTabs<IProjectInputMethod>(3,true);
+            inputTabs.AddTab("Folder",new MultiFolderDialog());
+            inputTabs.AddTab("GitHub",new MultiFolderDialog());
+            inputTabs.AddTab("Magic",new MultiFolderDialog());
+
+            inputTabs.Select += inputMethod => {
+                inputMethod.Run(new string[0]);
+                return true;
+            };
+
+            inputTabs.Render();
+            inputTabs.HandleInput();
+
+            Console.ReadKey();
         }
     }
 }
-
