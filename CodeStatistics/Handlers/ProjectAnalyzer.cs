6596ed0f3ea383213d23cd0b4254ef7f8f94a972
chylex <info@chylex.com>
2015-09-08 18:43:37 +0200

    Add ProjectAnalyzer that runs handlers, reports progress and generates tabs

---
+++ CodeStatistics/Handlers/ProjectAnalyzer.cs
@@ -0,0 +1,61 @@
+﻿using CodeStatistics.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeStatistics.Handlers{
+    class ProjectAnalyzer{
+        public delegate void UpdateEventHandler(float percentage, int handledFiles, int totalFiles);
+
+        public event UpdateEventHandler Update;
+
+        private readonly ICollection<File> files;
+
+        public ProjectAnalyzer(ICollection<File> files){
+            this.files = files;
+        }
+
+        public List<IHandlerTab> Run(){
+            int updateInterval = (int)Math.Ceiling(files.Count/200D), nextUpdate = updateInterval;
+            int handledFiles = 0, totalFiles = files.Count;
+            HashSet<FileHandler> handlers = new HashSet<FileHandler>();
+
+            if (Update != null)Update(0F,0,totalFiles);
+
+            foreach(File file in files){
+                FileHandler handler = FileHandlers.Get(file.Ext);
+
+                if (handler != null && handler.IsFileValid(file)){
+                    handler.Handle(file);
+                    handlers.Add(handler);
+                }
+
+                ++handledFiles;
+
+                if (--nextUpdate <= 0){
+                    nextUpdate = updateInterval;
+                    if (Update != null)Update((float)handledFiles/totalFiles,handledFiles,totalFiles);
+                }
+            }
+
+            if (Update != null)Update(1F,totalFiles,totalFiles);
+
+            // Major handler check
+            int majorHandlerCount = handlers.Count(handler => handler is FileHandler.Major);
+
+            if (majorHandlerCount > 1){
+                // TODO
+            }
+
+            // Tab list
+            List<IHandlerTab> tabs = new List<IHandlerTab>();
+            if (majorHandlerCount > 0)tabs.AddRange(handlers.Where(handler => handler is FileHandler.Major).First().GenerateTabs());
+
+            List<FileHandler> minorHandlers = handlers.Where(handler => handler is FileHandler.Minor).ToList();
+            minorHandlers.Sort();
+            minorHandlers.ForEach(handler => tabs.AddRange(handler.GenerateTabs()));
+
+            return tabs;
+        }
+    }
+}
--- CodeStatistics/Program.cs
+++ CodeStatistics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CodeStatistics.ConsoleUtil;
+using CodeStatistics.Handlers;
 using CodeStatistics.Input;
 using System;
 using System.Collections.Generic;
@@ -46,9 +47,38 @@ namespace CodeStatistics{
 
             Console.CursorVisible = false;
             HashSet<File> foundFiles = search.Search();
-            Console.CursorVisible = true;
 
             // File parsing
+            ProjectAnalyzer analyzer = new ProjectAnalyzer(foundFiles);
+
+            console.Clear();
+            console.SetForeground(ConsoleColor.White);
+            console.WriteCenter(1,"Processing files...");
+
+            analyzer.Update += (percentage, handledFiles, totalFiles) => {
+                console.ClearLine(3);
+                console.SetForeground(ConsoleColor.Yellow);
+                console.MoveToCenter(50,3);
+                console.Write(new string('▒',(int)Math.Floor(percentage*50F)));
+                console.SetForeground(ConsoleColor.Gray);
+                console.WriteCenter(5,handledFiles+" / "+totalFiles);
+            };
+
+            List<IHandlerTab> generatedTabs = analyzer.Run();
+
+            // Tab generation
+            const int y = 1;
+
+            ConsoleTabs<IHandlerTab> tabs = new ConsoleTabs<IHandlerTab>(y,false);
+            foreach(IHandlerTab tab in generatedTabs)tabs.AddTab(tab.GetName(),tab);
+
+            tabs.Select += tab => {
+                tab.RenderInfo(console,y+2);
+                return false;
+            };
+
+            tabs.Render();
+            tabs.HandleInput();
 
             // Pause
             Console.ReadKey();
