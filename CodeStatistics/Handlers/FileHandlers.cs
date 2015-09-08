0a1c46c078556b40395346b522133dcd76fac775
chylex <info@chylex.com>
2015-09-08 18:14:25 +0200

    Add a WIP asset handler

---
+++ CodeStatistics/Handlers/FileHandlers.cs
@@ -0,0 +1,42 @@
+﻿using CodeStatistics.Handlers.Objects;
+using System.Collections.Generic;
+
+namespace CodeStatistics.Handlers{
+    static class FileHandlers{
+        private static readonly Dictionary<string,FileHandler> handlers = new Dictionary<string,FileHandler>();
+
+        static FileHandlers(){
+            AssetHandler assetHandler = new AssetHandler(10);
+
+            foreach(string ext in new string[]{
+                "jpg", "jpeg", "gif", "png", "bmp", "ico", "icns", "tif", "tiff", "tga", "svg"
+            })handlers.Add(ext,assetHandler.SetType(ext,AssetHandler.Type.Image));
+
+            foreach(string ext in new string[]{
+                "wav", "mp3", "ogg", "flac", "aiff", "wma", "m4a", "aac", "mid", "mod"
+            })handlers.Add(ext,assetHandler.SetType(ext,AssetHandler.Type.Audio));
+
+            foreach(string ext in new string[]{
+                "mp4", "mkv", "mpg", "mpeg", "avi", "m4v", "mov", "wmv"
+            })handlers.Add(ext,assetHandler.SetType(ext,AssetHandler.Type.Video));
+
+            foreach(string ext in new string[]{
+                "txt", "md", "rtf", "pdf", "doc", "docx", "odt"
+            })handlers.Add(ext,assetHandler.SetType(ext,AssetHandler.Type.Text));
+
+            foreach(string ext in new string[]{
+                "json", "xml", "conf", "cfg", "ini", "yaml"
+            })handlers.Add(ext,assetHandler.SetType(ext,AssetHandler.Type.Configuration));
+
+            foreach(string ext in new string[]{
+                "zip", "rar", "tar", "gz", "tgz", "cab", "bz2", "bzip", "lz", "lzma", "arc", "pak"
+            })handlers.Add(ext,assetHandler.SetType(ext,AssetHandler.Type.Archive));
+        }
+
+        public static FileHandler Get(string extNoDot){
+            FileHandler handler;
+            return handlers.TryGetValue(extNoDot,out handler) ? handler : null;
+        }
+    }
+}
+
---
+++ CodeStatistics/Handlers/Objects/AssetHandler.cs
@@ -0,0 +1,43 @@
+﻿using CodeStatistics.ConsoleUtil;
+using CodeStatistics.Input;
+using System;
+using System.Collections.Generic;
+
+namespace CodeStatistics.Handlers.Objects{
+    class AssetHandler : FileHandler.Minor{
+        public enum Type{
+            Image, Audio, Video, Text, Configuration, Archive
+        }
+
+        private Dictionary<Type,List<string>> types = new Dictionary<Type,List<string>>();
+        private Dictionary<string,int> count = new Dictionary<string,int>();
+
+        public AssetHandler(int priority) : base(priority){
+            foreach(Type type in Enum.GetValues(typeof(Type)))types[type] = new List<string>();
+        }
+
+        public AssetHandler SetType(string extension, Type type){
+            types[type].Add(extension);
+            return this;
+        }
+
+        public override void Handle(File file){
+            int amount;
+            count[file.Ext] = count.TryGetValue(file.Ext,out amount) ? ++amount : 1;
+        }
+
+        public override IHandlerTab[] GenerateTabs(){
+            return new IHandlerTab[]{ new AssetTab() };
+        }
+
+        class AssetTab : IHandlerTab{
+            public string GetName(){
+                return "Assets";
+            }
+
+            public void RenderInfo(ConsoleWrapper c, int y){
+                // TODO
+            }
+        }
+    }
+}
