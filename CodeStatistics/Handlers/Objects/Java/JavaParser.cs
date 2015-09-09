55be3eb5d7e538d8d80c404800d08ceaaa9d91b7
chylex <info@chylex.com>
2015-09-09 17:24:13 +0200

    Work on JavaParser

--- CodeStatistics/Handlers/Objects/Java/JavaParser.cs
+++ CodeStatistics/Handlers/Objects/Java/JavaParser.cs
@@ -1,43 +1,60 @@
 ﻿using CodeStatistics.Input;
 using System.Text.RegularExpressions;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace CodeStatistics.Handlers.Objects.Java{
     static class JavaParser{
-        private static readonly Regex commentOneLine = new Regex(@"//.*",RegexOptions.Compiled);
-        private static readonly Regex commentMultiLine = new Regex(@"/\*(?:.|\n)*?\*/",RegexOptions.Compiled);
-
         public static void Parse(string fileContents, JavaStatistics stats){
-            string fileParsed = commentMultiLine.Replace(commentOneLine.Replace(fileContents,""),"");
+            string fileParsed = JavaParseUtils.commentMultiLine.Replace(JavaParseUtils.commentOneLine.Replace(fileContents,""),"");
             
             string[] linesPlain = fileContents.Split('\n').Select(line => line.TrimEnd()).Where(line => line.Length > 0).ToArray(); // lines are always \n
             string[] linesParsed = fileParsed.Split('\n').Select(line => line.TrimEnd()).Where(line => line.Length > 0).ToArray(); //  ^
 
-            // Package
-            string package = null;
-            if (linesParsed.FirstOrDefault(line => line.ExtractBoth("package ",";",out package)) == null)return; // should not happen, but what if...
+            // Final variables
+            string _package = null;
+            JavaType _currentType = JavaType.Invalid;
+            string _simpleType = null;
+            string _fullType;
+            int _totalLines;
+            int _totalCharacters;
 
-            stats.Packages.Add(package);
+            // Package
+            if (linesParsed.FirstOrDefault(line => line.ExtractBoth("package ",";",out _package)) == null)return; // should not happen, but what if...
+
+            stats.Packages.Add(_package);
+
+            // Current type
+            string typeLine = linesParsed.FirstOrDefault(line => JavaParseUtils.typeIdentifiers.Any(identifier => line.Contains(identifier)));
+            if (typeLine == null)return; // should not happen either, but we know the drill...
+
+            KeyValuePair<JavaType,string> typeData = JavaParseUtils.GetType(typeLine);
+            _currentType = typeData.Key;
+            _simpleType = typeData.Value;
+            _fullType = _package+"."+_simpleType;
 
             // Lines
-            int totalLines = linesPlain.Count(line => !line.TrimStart().Equals("{"));
+            _totalLines = linesPlain.Count(line => !line.TrimStart().Equals("{"));
 
-            stats.LinesTotal += totalLines;
+
+
+            stats.LinesTotal += _totalLines;
 
             // Characters
             int spaces = 0;
 
-            int totalCharacters = linesPlain.Select(line => {
+            _totalCharacters = linesPlain.Select(line => {
                 string noSpaces = line.TrimStart(' ');
+
                 int diff = line.Length-noSpaces.Length;
-
                 if (diff > 0 && spaces == 0)spaces = diff;
-                if (diff > 0)line = noSpaces.PadLeft(noSpaces.Length+diff/spaces,'\t'); // replaces spaces with tabs
 
-                return line.Length;
+                return diff > 0 ? noSpaces.PadLeft(noSpaces.Length+diff/spaces,'\t').Length : line.Length; // replaces spaces with tabs if there are any
             }).Sum();
 
-            stats.CharactersTotal += totalCharacters;
+
+
+            stats.CharactersTotal += _totalCharacters;
         }
     }
 }
