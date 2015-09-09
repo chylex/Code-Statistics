25ebe4230cb17bd7dbd01c1a8b3ae5484c9c4a90
chylex <info@chylex.com>
2015-09-09 21:01:43 +0200

    Move Asset tab to a separate class and center all columns

--- CodeStatistics/Handlers/Objects/AssetHandler.cs
+++ CodeStatistics/Handlers/Objects/AssetHandler.cs
@@ -1,4 +1,4 @@
-﻿using CodeStatistics.ConsoleUtil;
+﻿using CodeStatistics.Handlers.Objects.Assets;
 using CodeStatistics.Input;
 using System;
 using System.Collections.Generic;
@@ -10,7 +10,7 @@ namespace CodeStatistics.Handlers.Objects{
             Image, Text, Video, Configuration, Audio, Archive
         }
 
-        private static string[] typeNames = new string[]{ "Images", "Text", "Video", "Configuration", "Audio", "Archives" };
+        public static readonly string[] TypeNames = new string[]{ "Images", "Text", "Video", "Configuration", "Audio", "Archives" };
 
         private Dictionary<Type,List<string>> types = new Dictionary<Type,List<string>>();
         private Dictionary<string,int> count = new Dictionary<string,int>();
@@ -29,68 +29,21 @@ namespace CodeStatistics.Handlers.Objects{
             count[file.Ext] = count.TryGetValue(file.Ext,out amount) ? ++amount : 1;
         }
 
-        private int GetExtCount(string ext){
+        public List<string> GetAssetExtensions(Type type){
+            return types[type];
+        }
+
+        public int GetAssetCount(){
+            return count.Values.Sum();
+        }
+
+        public int GetExtCount(string ext){
             int amount;
             return count.TryGetValue(ext,out amount) ? amount : 0;
         }
 
         public override IHandlerTab[] GenerateTabs(){
             return new IHandlerTab[]{ new AssetTab(this) };
-        }
-
-        class AssetTab : IHandlerTab{
-            private AssetHandler handler;
-
-            public AssetTab(AssetHandler handler){
-                this.handler = handler;
-            }
-
-            public string GetName(){
-                return "Assets";
-            }
-
-            public void RenderInfo(ConsoleWrapper c, int y){
-                c.SetForeground(ConsoleColor.White);
-                c.WriteCenter(y,"Total Assets: ");
-                c.SetForeground(ConsoleColor.Gray);
-                c.Write(handler.count.Values.Sum().ToString());
-                
-                Array types = Enum.GetValues(typeof(Type));
-                const int dist = 28;
-
-                int px = c.Width/2-dist-5, py = y+3;
-
-                for(int ind = 0; ind < types.Length; ind++){
-                    Type currentType = (Type)types.GetValue(ind);
-                    List<string> exts = handler.types[currentType];
-
-                    IEnumerable<KeyValuePair<string,int>> usedExts = exts.Select(ext => new KeyValuePair<string,int>(ext,handler.GetExtCount(ext))).Where(kvp => kvp.Value != 0);
-
-                    int total = usedExts.Select(kvp => kvp.Value).DefaultIfEmpty(0).Sum();
-                    int longestExt = usedExts.Select(kvp => kvp.Key.Length).DefaultIfEmpty(0).Max();
-
-                    c.SetForeground(ConsoleColor.Yellow);
-                    c.Write(px,py,typeNames[ind]+": ");
-                    c.SetForeground(ConsoleColor.Gray);
-                    c.Write(total.ToString());
-
-                    foreach(KeyValuePair<string,int> kvp in usedExts.OrderByDescending(kvp => kvp.Value).ToList()){
-                        c.MoveTo(px,++py);
-                        c.SetForeground(ConsoleColor.White);
-                        c.Write(".{0}: ",kvp.Key);
-                        c.SetForeground(ConsoleColor.Gray);
-                        c.Write(new string(' ',longestExt-kvp.Key.Length));
-                        c.Write("{0} ({1}%)",kvp.Value,Math.Round(100*(decimal)kvp.Value/total,2));
-                    }
-
-                    c.MoveTo(px,py += 3);
-
-                    if (ind%2 != 0){
-                        px += dist;
-                        py = y+3;
-                    }
-                }
-            }
         }
     }
 }
---
+++ CodeStatistics/Handlers/Objects/Assets/AssetTab.cs
@@ -0,0 +1,90 @@
+﻿using CodeStatistics.ConsoleUtil;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CodeStatistics.Handlers.Objects.Assets{
+    class AssetTab : IHandlerTab{
+        private static readonly AssetHandler.Type[] types = Enum.GetValues(typeof(AssetHandler.Type)).Cast<AssetHandler.Type>().ToArray();
+
+        private readonly int totalAssets;
+        private readonly Dictionary<AssetHandler.Type,AssetTypeInfo> info = new Dictionary<AssetHandler.Type,AssetTypeInfo>();
+
+        public AssetTab(AssetHandler handler){
+            totalAssets = handler.GetAssetCount();
+
+            foreach(AssetHandler.Type type in types){
+                List<string> exts = handler.GetAssetExtensions(type);
+                info[type] = new AssetTypeInfo(exts.Select(ext => new KeyValuePair<string,int>(ext,handler.GetExtCount(ext))).Where(kvp => kvp.Value != 0));
+            }
+        }
+
+        public string GetName(){
+            return "Assets";
+        }
+
+        public void RenderInfo(ConsoleWrapper c, int y){
+            c.SetForeground(ConsoleColor.White);
+            c.WriteCenter(y,"Total Assets: ");
+            c.SetForeground(ConsoleColor.Gray);
+            c.Write(totalAssets.ToString());
+            
+            const int dist = 28;
+
+            int py = y+3;
+            int[] columnWidths = new int[]{ 0, 0, 0 };
+
+            for(int ind = 0, col = 0, width = 0; ind < types.Length; ind++, col = ind/2){
+                AssetTypeInfo typeInfo = info[types[ind]];
+
+                width = AssetHandler.TypeNames[ind].Length+2+typeInfo.Total.ToString().Length;
+                if (width > columnWidths[col])columnWidths[col] = width;
+
+                foreach(KeyValuePair<string,int> kvp in typeInfo.ExtCount){
+                    width = kvp.Key.Length+(typeInfo.MaxLength-kvp.Key.Length)+kvp.Value.ToString().Length+Math.Round(100*(decimal)kvp.Value/typeInfo.Total,2).ToString().Length+6;
+                    if (width > columnWidths[col])columnWidths[col] = width;
+                }
+            }
+
+            for(int ind = 0, col = 0; ind < types.Length; ind++, col = ind/2){
+                int px = c.Width/2+(col-1)*dist-(columnWidths[col]/2);
+
+                AssetTypeInfo typeInfo = info[types[ind]];
+
+                c.SetForeground(ConsoleColor.Yellow);
+                c.Write(px,py,AssetHandler.TypeNames[ind]+": ");
+                c.SetForeground(ConsoleColor.Gray);
+                c.Write(typeInfo.Total.ToString());
+
+                foreach(KeyValuePair<string,int> kvp in typeInfo.ExtCount.OrderByDescending(kvp => kvp.Value)){
+                    c.MoveTo(px,++py);
+                    c.SetForeground(ConsoleColor.White);
+                    c.Write(".{0}: ",kvp.Key);
+                    c.SetForeground(ConsoleColor.Gray);
+                    c.Write(new string(' ',typeInfo.MaxLength-kvp.Key.Length));
+                    c.Write("{0} ({1}%)",kvp.Value,Math.Round(100*(decimal)kvp.Value/typeInfo.Total,2));
+                }
+
+                c.MoveTo(px,py += 3);
+
+                if (ind%2 != 0){
+                    px += dist;
+                    py = y+3;
+                }
+            }
+        }
+
+        struct AssetTypeInfo{
+            public readonly IEnumerable<KeyValuePair<string,int>> ExtCount;
+            public readonly int Total;
+            public readonly int MaxLength;
+
+            public AssetTypeInfo(IEnumerable<KeyValuePair<string,int>> extCount){
+                this.ExtCount = extCount;
+                this.Total = extCount.Select(kvp => kvp.Value).DefaultIfEmpty(0).Sum();
+                this.MaxLength = extCount.Select(kvp => kvp.Key.Length).DefaultIfEmpty(0).Max();
+            }
+        }
+    }
+}
