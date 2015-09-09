4affa86f00ab8c387a066a0ca780440987ba17f4
chylex <info@chylex.com>
2015-09-09 17:23:29 +0200

    Move Java parsing methods and fields to a separate class

---
+++ CodeStatistics/Handlers/Objects/Java/JavaParseUtils.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace CodeStatistics.Handlers.Objects.Java{
+    static class JavaParseUtils{
+        public static readonly Regex commentOneLine = new Regex(@"//.*",RegexOptions.Compiled);
+        public static readonly Regex commentMultiLine = new Regex(@"/\*(?:.|\n)*?\*/",RegexOptions.Compiled);
+
+        public static readonly string[] typeIdentifiers = new string[]{ "class", "@interface", "interface", "enum" };
+        public static readonly string[] modifiers = new string[]{ "public", "protected", "private", "static", "final", "abstract", "synchronized", "volatile", "native", "transient", "strictfp" };
+
+        /// <summary>
+        /// Checks whether the identifier consists of letters, numbers, $ and _.
+        /// If you are using stuff like diacritics, ₯, ௹, or  ⁀  in java identifiers, stop.
+        /// </summary>
+        public static bool IsJavaIdentifier(string text){
+            for(int ind = 0; ind < text.Length; ind++){
+                char chr = text[ind];
+                if (!char.IsLetter(chr) && !(char.IsDigit(chr) && ind > 0) && chr != '_' && chr != '$')return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Removes modifiers from the line, including the space after them.
+        /// </summary>
+        public static string StripModifiers(string line){
+            string[] found;
+            return StripModifiers(line,out found);
+        }
+
+        /// <summary>
+        /// Removes modifiers from the line, including the space after them, and returns all found modifiers.
+        /// </summary>
+        public static string StripModifiers(string line, out string[] found){
+            List<string> foundModifiers = new List<string>();
+
+            for(int modifierInd = 0, index; modifierInd < modifiers.Length; modifierInd++){
+                string modifier = modifiers[modifierInd];
+
+                if ((index = line.IndexOf(modifier)) != -1){
+                    foundModifiers.Add(modifier);
+                    line = line.Remove(index,modifier.Length+1); // remove space
+                }
+            }
+
+            found = foundModifiers.ToArray();
+            return line;
+        }
+
+        /// <summary>
+        /// Finds the java type and its name in the line. If the found text is not a valid java identifier, a KeyValuePair with JavaType.Invalid is returned.
+        /// </summary>
+        public static KeyValuePair<JavaType,string> GetType(string line){
+            line = StripModifiers(line.RemoveFrom(" extends").RemoveFrom(" implements").ExtractEnd("{")).TrimStart();
+
+            string type = typeIdentifiers.FirstOrDefault(identifier => line.StartsWith(identifier));
+            line = line.Substring(type.Length+1).Trim();
+
+            if (IsJavaIdentifier(line)){
+                JavaType javaType = type.Equals("class") ? JavaType.Class :
+                                    type.Equals("interface") ? JavaType.Interface :
+                                    type.Equals("enum") ? JavaType.Enum :
+                                    type.Equals("@interface") ? JavaType.Annotation : JavaType.Invalid;
+                return new KeyValuePair<JavaType,string>(javaType,line);
+            }
+            else return new KeyValuePair<JavaType,string>(JavaType.Invalid,null);
+        }
+    }
+}
--- CodeStatistics/Tests/JavaParserTests.cs
+++ CodeStatistics/Tests/JavaParserTests.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using CodeStatistics.Handlers.Objects.Java;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace CodeStatistics.Tests{
     [TestClass]
     public class JavaParserTests{
         [TestMethod]
-        public void TestPackage(){
+        public void TestJavaIdentifiers(){
+            Assert.IsTrue(JavaParseUtils.IsJavaIdentifier("ClassName"),"ClassName is supposed to be valid");
+            Assert.IsTrue(JavaParseUtils.IsJavaIdentifier("ClassName123"),"ClassName123 is supposed to be valid");
+            Assert.IsTrue(JavaParseUtils.IsJavaIdentifier("myMethod"),"myMethod is supposed to be valid");
+            Assert.IsTrue(JavaParseUtils.IsJavaIdentifier("my_method$"),"my_method$ is supposed to be valid");
+
+            Assert.IsFalse(JavaParseUtils.IsJavaIdentifier("0ClassName123"),"0ClassName123 is supposed to be invalid");
+            Assert.IsFalse(JavaParseUtils.IsJavaIdentifier("Class Name"),"Class Name is supposed to be invalid");
+            Assert.IsFalse(JavaParseUtils.IsJavaIdentifier("Class.Name"),"Class.Name is supposed to be invalid");
+        }
+
+        [TestMethod]
+        public void TestGetType(){
+            KeyValuePair<JavaType,string> kvp;
             
+            kvp = JavaParseUtils.GetType("public class MyClassName implements Things");
+            Assert.AreEqual(JavaType.Class,kvp.Key);
+            Assert.AreEqual("MyClassName",kvp.Value);
+
+            kvp = JavaParseUtils.GetType("interface MyInterface extends Stuff");
+            Assert.AreEqual(JavaType.Interface,kvp.Key);
+            Assert.AreEqual("MyInterface",kvp.Value);
+
+            kvp = JavaParseUtils.GetType("private enum Enum{");
+            Assert.AreEqual(JavaType.Enum,kvp.Key);
+            Assert.AreEqual("Enum",kvp.Value);
+
+            kvp = JavaParseUtils.GetType("public @interface MyAnnotation {");
+            Assert.AreEqual(JavaType.Annotation,kvp.Key);
+            Assert.AreEqual("MyAnnotation",kvp.Value);
         }
     }
 }
