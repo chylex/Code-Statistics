599215391d8376b9fceac53b7e8138e1b1053d24
chylex <info@chylex.com>
2015-09-09 17:22:35 +0200

    Add JavaType enum for types

---
+++ CodeStatistics/Handlers/Objects/Java/JavaType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeStatistics.Handlers.Objects.Java{
+    enum JavaType{
+        Invalid,
+        Class,
+        Interface,
+        Enum,
+        Annotation
+    }
+}
