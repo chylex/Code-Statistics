28b4246586d1ea4b6ebae8f491f3e2e42888e14c
chylex <info@chylex.com>
2015-09-09 13:46:54 +0200

    Fix Java line splitting and remove \r from comment regex

--- CodeStatistics/Handlers/Objects/Java/JavaParser.cs
+++ CodeStatistics/Handlers/Objects/Java/JavaParser.cs
@@ -4,14 +4,14 @@ using System.Text.RegularExpressions;
 namespace CodeStatistics.Handlers.Objects.Java{
     static class JavaParser{
         private static readonly Regex commentOneLine = new Regex(@"//.*",RegexOptions.Compiled);
-        private static readonly Regex commentMultiLine = new Regex(@"/\*(?:.|[\n\r])*?\*/",RegexOptions.Compiled);
+        private static readonly Regex commentMultiLine = new Regex(@"/\*(?:.|\n)*?\*/",RegexOptions.Compiled);
 
         public static void Parse(File file, JavaStatistics stats){
             string filePlain = file.Read();
             string fileParsed = commentMultiLine.Replace(commentOneLine.Replace(filePlain,""),"");
-
-            string[] linesPlain = filePlain.Split("\\n".ToCharArray()); // lines are always \n
-            string[] linesParsed = fileParsed.Split("\\n".ToCharArray()); // ^
+            
+            string[] linesPlain = filePlain.Split('\n'); // lines are always \n
+            string[] linesParsed = fileParsed.Split('\n'); // ^
 
 
         }
