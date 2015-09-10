85eddbd8c4b9f6f65aabe036cdf6f9dde4842d87
chylex <info@chylex.com>
2015-09-10 18:38:29 +0200

    Add a Triple collection

---
+++ CodeStatistics/Collections/Triple.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeStatistics.Collections{
+    struct Triple<A, B, C>{
+        public readonly A First;
+        public readonly B Second;
+        public readonly C Third;
+
+        public Triple(A first, B second, C third){
+            this.First = first;
+            this.Second = second;
+            this.Third = third;
+        }
+    }
+}
