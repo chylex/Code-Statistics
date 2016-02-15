f35c2dde814d604f1e62a3cd07ea56aca610ecfd
chylex <info@chylex.com>
2016-02-16 00:59:35 +0100

    Add AnonymousDictionary utility class to create dicts from anonymous types

---
+++ CodeStatistics/Collections/AnonymousDictionary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CodeStatistics.Collections{
+    static class AnonymousDictionary{
+        public static Dictionary<string,TValue> Create<TValue>(object obj){
+            var dict = new Dictionary<string,TValue>();
+
+            foreach(PropertyDescriptor descriptor in TypeDescriptor.GetProperties(obj)){
+                dict.Add(descriptor.Name,(TValue)descriptor.GetValue(obj));
+            }
+
+            return dict;
+        }
+    }
+}
