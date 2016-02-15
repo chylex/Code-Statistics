2f335bc6a808914da727e137a57db7124ef0c47a
chylex <info@chylex.com>
2016-02-16 00:21:54 +0100

    Add dummy methods to Variables class

--- CodeStatistics/Handling/Variables.cs
+++ CodeStatistics/Handling/Variables.cs
@@ -1,5 +1,17 @@
-﻿namespace CodeStatistics.Handling{
+﻿using System.Collections.Generic;
+
+namespace CodeStatistics.Handling{
     class Variables{
-        // TODO
+        public bool CheckFlag(string name){
+            return false;
+        }
+
+        public string GetVariable(string name){
+            return "";
+        }
+
+        public IEnumerable<Variables> GetArray(string name){
+            yield break;
+        }
     }
 }
