fe6ab46cb40ad762eb4838695aba1ec060ef0523
chylex <info@chylex.com>
2016-02-16 00:59:43 +0100

    Implement variable handling

--- CodeStatistics/Handling/Variables.cs
+++ CodeStatistics/Handling/Variables.cs
@@ -1,17 +1,80 @@
-﻿using System.Collections.Generic;
+﻿using CodeStatistics.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CodeStatistics.Handling{
-    class Variables{
-        public bool CheckFlag(string name){
-            return false;
+    abstract class Variables{
+        public abstract bool CheckFlag(string name);
+        public abstract string GetVariable(string name);
+        public abstract IEnumerable<Variables> GetArray(string name);
+
+        public class Root : Variables{
+            private readonly List<string> flags = new List<string>(4);
+            private readonly Dictionary<string,string> variables = new Dictionary<string,string>();
+            private readonly Dictionary<string,List<Variables>> arrays = new Dictionary<string,List<Variables>>();
+            
+            public void AddFlag(string name){
+                flags.Add(name);
+            }
+
+            public override bool CheckFlag(string name){
+                return flags.Contains(name);
+            }
+
+            public void SetVariable(string name, string value){
+                variables[name] = value;
+            }
+
+            public void SetVariable(string name, object value){
+                variables[name] = value.ToString();
+            }
+
+            public override string GetVariable(string name){
+                string value;
+                return variables.TryGetValue(name,out value) ? value : "";
+            }
+
+            public void AddToArray(string name, object array){
+                List<Variables> arrayList;
+
+                if (arrays.ContainsKey(name)){
+                    arrayList = arrays[name];
+                }
+                else{
+                    arrayList = new List<Variables>(4);
+                    arrays[name] = arrayList;
+                }
+
+                arrayList.Add(new AdapterArrayVariables(this,array));
+            }
+
+            public override IEnumerable<Variables> GetArray(string name){
+                List<Variables> arrayList;
+                return arrays.TryGetValue(name,out arrayList) ? arrayList : Enumerable.Empty<Variables>();
+            }
         }
 
-        public string GetVariable(string name){
-            return "";
-        }
+        private class AdapterArrayVariables : Variables{
+            private readonly Variables parent;
+            private readonly Dictionary<string,string> variables;
 
-        public IEnumerable<Variables> GetArray(string name){
-            yield break;
+            public AdapterArrayVariables(Variables parent, object array){
+                this.parent = parent;
+                this.variables = AnonymousDictionary.Create<string>(array);
+            }
+
+            public override bool CheckFlag(string name){
+                return parent.CheckFlag(name);
+            }
+
+            public override string GetVariable(string name){
+                string value;
+                return variables.TryGetValue(name,out value) ? value : "";
+            }
+
+            public override IEnumerable<Variables> GetArray(string name){
+                return parent.GetArray(name);
+            }
         }
     }
 }
