ac1d988f159cf999a102d3e4000298f9fc1bcb2e
chylex <info@chylex.com>
2016-02-16 00:09:19 +0100

    Add an HTML output generator using the template engine

---
+++ CodeStatistics/Output/GenerateHtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using CodeStatistics.Handling;
+using System.IO;
+using System.Text;
+
+namespace CodeStatistics.Output{
+    class GenerateHtml{
+        private readonly TemplateList templates;
+        private readonly Variables variables;
+
+        public GenerateHtml(IList<string> templateLines, Variables variables){
+            this.templates = new TemplateList(templateLines);
+            this.variables = variables;
+        }
+
+        public bool ToFile(string file){
+            string contents = templates.ProcessTemplate("html",variables);
+
+            try{
+                File.WriteAllText(file,contents,Encoding.UTF8);
+                return File.Exists(file);
+            }catch(Exception){
+                return false;
+            }
+        }
+    }
+}
