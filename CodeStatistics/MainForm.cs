61b26375a32121e1e47dfa4816f103cf9e8935f1
chylex <info@chylex.com>
2016-02-16 18:12:06 +0100

    Add file/folder drag and drop support on the main form

--- CodeStatistics/MainForm.cs
+++ CodeStatistics/MainForm.cs
@@ -9,6 +9,29 @@ namespace CodeStatistics{
             InitializeComponent();
         }
 
+        // Drag Events
+
+        private void OnDragEnter(object sender, DragEventArgs e){
+            e.Effect = e.Data.GetDataPresent("FileDrop") ? DragDropEffects.Copy : DragDropEffects.None;
+        }
+
+        private void OnDragDrop(object sender, DragEventArgs e){
+            if (e.Data.GetDataPresent("FileDrop")){
+                object fileDropData = e.Data.GetData("FileDrop");
+                string[] files = fileDropData as string[];
+
+                if (files == null){
+                    string file = fileDropData as string;
+                    if (file != null)files = new[]{ file };
+                }
+
+                if (files == null || files.Length == 0)return;
+
+                // TODO
+            }
+        }
+
+        // Button Click Events
 
         private void btnProjectFolder_Click(object sender, EventArgs e){
             string[] folders = MultiFolderDialog.Show(this);
@@ -17,6 +40,7 @@ namespace CodeStatistics{
                 // TODO
             }
         }
+
         private void btnViewSourceCode_Click(object sender, EventArgs e){
             Process.Start("https://github.com/chylex/Code-Statistics");
         }
