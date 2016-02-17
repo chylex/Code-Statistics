fad8a4640f591eeb6ae35e60129f01fbbfae4aa5
chylex <info@chylex.com>
2016-02-17 16:30:53 +0100

    Add a Project class to handle all file processing and variable generation

---
+++ CodeStatistics/Handling/Project.cs
@@ -0,0 +1,90 @@
+﻿using CodeStatistics.Input;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+// ReSharper disable AccessToModifiedClosure
+namespace CodeStatistics.Handling{
+    class Project{
+        public delegate void ProgressEventHandler(int percentage, int processedEntries, int totalEntries);
+        public delegate void FinishEventHandler(Variables variables);
+
+        public event ProgressEventHandler Progress;
+        public event FinishEventHandler Finish;
+
+        private readonly FileSearchData searchData;
+        private readonly CancellationTokenSource cancelToken;
+
+        public Project(FileSearchData searchData){
+            this.searchData = searchData;
+            this.cancelToken = new CancellationTokenSource();
+        }
+
+        public void ProcessAsync(){
+            new Task(() => {
+                var variables = new Variables.Root();
+                
+                int processedEntries = 0, totalEntries = searchData.EntryCount;
+                int processedWeight = 0, totalWeight = HandlerList.GetTotalWeight(searchData);
+
+                int updateCounter = 0, updateInterval = totalEntries/400;
+
+                if (Progress != null)Progress(0,processedEntries,totalEntries);
+
+                Action checkProgress;
+
+                if (Progress != null){
+                    checkProgress = () => {
+                        if (++updateCounter > updateInterval && Progress != null){
+                            updateCounter = 0;
+                            Progress((int)Math.Floor(100F*processedWeight/totalWeight),processedEntries,totalEntries);
+                        }
+                    };
+                }
+                else{
+                    checkProgress = () => {};
+                }
+                
+                // Folders
+                List<IFolderHandler> folderHandlers = HandlerList.GetFolderHandlers().ToList();
+                int folderHandlerWeight = folderHandlers.Sum(handler => handler.Weight);
+
+                foreach(string folder in searchData.Folders){
+                    if (cancelToken.IsCancellationRequested)return;
+
+                    foreach(IFolderHandler folderHandler in folderHandlers){
+                        folderHandler.Process(folder,variables);
+                    }
+
+                    ++processedEntries;
+                    ++updateCounter;
+                    processedWeight += folderHandlerWeight;
+                    checkProgress();
+                }
+
+                // Files
+                foreach(File file in searchData.Files){
+                    if (cancelToken.IsCancellationRequested)return;
+
+                    IFileHandler handler = HandlerList.GetFileHandler(file);
+                    handler.Process(file,variables);
+
+                    ++processedEntries;
+                    ++updateCounter;
+                    processedWeight += handler.Weight;
+                    checkProgress();
+                }
+
+                // Finalize
+                if (Progress != null)Progress(100,processedEntries,totalEntries);
+                if (Finish != null)Finish(variables);
+            },cancelToken.Token).Start();
+        }
+
+        public void Cancel(){
+            cancelToken.Cancel(false);
+        }
+    }
+}
