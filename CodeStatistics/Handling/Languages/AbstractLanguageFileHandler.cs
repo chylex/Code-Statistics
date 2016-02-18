73f7c5918ae3792e565fa6dd4dd8192448b5b1ed
chylex <info@chylex.com>
2016-02-18 02:50:58 +0100

    Add total line and character count with minor refactorings to language handlers

--- CodeStatistics/Handling/Languages/AbstractLanguageFileHandler.cs
+++ CodeStatistics/Handling/Languages/AbstractLanguageFileHandler.cs
@@ -3,9 +3,31 @@ using CodeStatistics.Input;
 
 namespace CodeStatistics.Handling.Languages{
     abstract class AbstractLanguageFileHandler : AbstractFileHandler{
+        protected abstract string Key { get; }
+
         public override void Process(File file, Variables.Root variables){
             base.Process(file,variables);
+            variables.AddFlag(Key);
             variables.Increment("fileTypeCode");
+
+            ProcessFileContents(file,variables);
+        }
+
+        protected virtual void ProcessFileContents(File file, Variables.Root variables){
+            string[] contents = file.Contents;
+
+            int lineCount = 0, charCount = 0;
+
+            foreach(string line in contents){
+                if (!line.Trim().Equals("{")){
+                    ++lineCount;
+                }
+
+                charCount += line.Length;
+            }
+
+            variables.Increment(Key+"LinesTotal",lineCount);
+            variables.Increment(Key+"CharsTotal",charCount);
         }
     }
 }
--- CodeStatistics/Handling/Languages/JavaHandler.cs
+++ CodeStatistics/Handling/Languages/JavaHandler.cs
@@ -1,10 +1,17 @@
-﻿using CodeStatistics.Handling.General;
-using CodeStatistics.Input;
+﻿using CodeStatistics.Input;
 
 namespace CodeStatistics.Handling.Languages {
     class JavaHandler : AbstractLanguageFileHandler{
         public override int Weight{
             get { return 50; }
+        }
+
+        protected override string Key{
+            get { return "java"; }
+        }
+
+        public override void SetupProject(Variables.Root variables){
+            base.SetupProject(variables);
         }
 
         public override void Process(File file, Variables.Root variables){
