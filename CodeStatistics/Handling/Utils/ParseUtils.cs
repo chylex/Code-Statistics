feff908b7073fee5389ed81490ab5aec4254762f
chylex <info@chylex.com>
2016-02-19 16:22:52 +0100

    Add argument check to ParseUtils.CountCharacters

--- CodeStatistics/Handling/Utils/ParseUtils.cs
+++ CodeStatistics/Handling/Utils/ParseUtils.cs
@@ -1,11 +1,14 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace CodeStatistics.Handling.Utils{
     public static class ParseUtils{
         /// <summary>
-        /// Returns the amount of characters in a string after converting leading spaces to tabs.
+        /// Returns the amount of characters in a string after converting leading spaces to tabs. Amount of spaces per tab has to be equal to or larger than 1.
         /// </summary>
         public static int CountCharacters(string line, int spacesPerTab = 4){
+            if (spacesPerTab < 1)throw new ArgumentException("spacesPerTab");
+
             int spaces = line.Length-line.SkipWhile(chr => chr == ' ').Count();
             return line.Length-spaces*(spacesPerTab-1)/spacesPerTab;
         }
