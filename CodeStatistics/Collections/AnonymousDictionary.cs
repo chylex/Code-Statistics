2fbd8bc90cb61bbdc46c7748b5bff030336779dc
chylex <info@chylex.com>
2016-02-19 13:07:45 +0100

    Add formatting capabilities to AnonymousDictionary<string,string>

--- CodeStatistics/Collections/AnonymousDictionary.cs
+++ CodeStatistics/Collections/AnonymousDictionary.cs
@@ -3,6 +3,8 @@ using System.ComponentModel;
 
 namespace CodeStatistics.Collections{
     static class AnonymousDictionary{
+        public delegate string ToStringFunction(object obj);
+
         public static Dictionary<string,TValue> Create<TValue>(object obj){
             var dict = new Dictionary<string,TValue>();
 
@@ -14,10 +16,14 @@ namespace CodeStatistics.Collections{
         }
 
         public static Dictionary<string,string> Create(object obj){
+            return Create(obj,o => o.ToString());
+        }
+
+        public static Dictionary<string,string> Create(object obj, ToStringFunction toString){
             var dict = new Dictionary<string,string>();
 
             foreach(PropertyDescriptor descriptor in TypeDescriptor.GetProperties(obj)){
-                dict.Add(descriptor.Name,descriptor.GetValue(obj).ToString());
+                dict.Add(descriptor.Name,toString(descriptor.GetValue(obj)));
             }
 
             return dict;
