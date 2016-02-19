08180e422b879a1540d46eba7b4c438ff0c421a4
chylex <info@chylex.com>
2016-02-19 15:54:30 +0100

    Create a new ParseUtils class with methods specific to code

---
+++ CodeStatistics/Handling/Utils/ParseUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace CodeStatistics.Handling.Utils{
+    public static class ParseUtils{
+        /// <summary>
+        /// Returns the amount of characters in a string after converting leading spaces to tabs.
+        /// </summary>
+        public static int CountCharacters(string line, int spacesPerTab = 4){
+            int spaces = line.Length-line.SkipWhile(chr => chr == ' ').Count();
+            return line.Length-spaces*(spacesPerTab-1)/spacesPerTab;
+        }
+    }
+}
