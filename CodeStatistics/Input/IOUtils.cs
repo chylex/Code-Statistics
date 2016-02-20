da15893b3404a5f349d6a011dcd4f912fdf2d07c
chylex <info@chylex.com>
2016-02-20 15:39:02 +0100

    Add IOUtils with file/directory related functions (+unit tests)

---
+++ CodeStatistics/Input/IOUtils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FileIO = System.IO.File;
+
+namespace CodeStatistics.Input{
+    public static class IOUtils{
+        /// <summary>
+        /// Finds the longest common path in an array. Does not include the trailing path separator, except when only the drive letter is returned. <para/>
+        /// If a path includes a file, the returned path will also include it if it's present in all other paths. <para/>
+        /// 
+        /// For example, <code>FindRootPath({ "C:\Projects\One", "C:\Projects\Two" })</code> returns C:\Projects
+        /// </summary>
+        public static string FindRootPath(string[] paths){
+            if (paths.Length == 0)throw new ArgumentException("paths");
+            if (paths.Length == 1)return paths[0];
+
+            List<string[]> split = paths.Select(path => path.Split(new []{ Path.DirectorySeparatorChar },StringSplitOptions.RemoveEmptyEntries)).ToList();
+            int commonElements = 0, maxCount = split.Min(path => path.Length);
+
+            for(; commonElements < maxCount; commonElements++){
+                string testElement = split[0][commonElements];
+
+                if (!split.Skip(1).All(path => path[commonElements].Equals(testElement))){
+                    break;
+                }
+            }
+
+            string commonPath = string.Join(Path.DirectorySeparatorChar.ToString(),split[0].Take(commonElements));
+            return commonElements == 1 && commonPath[commonPath.Length-1] == ':' ? commonPath+Path.DirectorySeparatorChar : commonPath;
+        }
+
+        /// <summary>
+        /// Returns true if the path points to an existing directory, false if it points to a file, and null if an exception happens during the IO operations.
+        /// </summary>
+        public static bool? IsDirectory(string path){
+            try{
+                return FileIO.GetAttributes(path).HasFlag(FileAttributes.Directory);
+            }catch(Exception){
+                return null;
+            }
+        }
+    }
+}
---
+++ CodeStatisticsTests/IOUtilsTests.cs
@@ -0,0 +1,28 @@
+﻿using CodeStatistics.Input;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeStatisticsTests{
+    [TestClass]
+    public class IOUtilsTests{
+        [TestMethod]
+        public void TestFindRootPath(){
+            Assert.AreEqual(@"C:\Projects", IOUtils.FindRootPath(new []{ @"C:\Projects" }));
+            Assert.AreEqual(@"C:\Projects\file.html", IOUtils.FindRootPath(new []{ @"C:\Projects\file.html" }));
+            Assert.AreEqual(@"C:\Projects\file.html", IOUtils.FindRootPath(new []{ @"C:\Projects\file.html", @"C:\Projects\file.html" }));
+
+            Assert.AreEqual(@"C:\Projects", IOUtils.FindRootPath(new []{ @"C:\Projects\MyProject", @"C:\Projects\OtherProject" }));
+            Assert.AreEqual(@"C:\Projects", IOUtils.FindRootPath(new []{ @"C:\Projects\MyProject\Build", @"C:\Projects\MyProject\Code", @"C:\Projects\OtherProject\Code" }));
+            Assert.AreEqual(@"C:\Projects\MyProject", IOUtils.FindRootPath(new []{ @"C:\Projects\MyProject", @"C:\Projects\MyProject\Build", @"C:\Projects\MyProject\Code" }));
+
+            Assert.AreEqual(@"C:\Projects", IOUtils.FindRootPath(new []{ @"C:\Projects\", @"C:\Projects\" }));
+            Assert.AreEqual(@"C:\Projects", IOUtils.FindRootPath(new []{ @"C:\\\Projects\", @"C:\\Projects\\" }));
+            Assert.AreEqual(@"C:\", IOUtils.FindRootPath(new []{ @"C:\", @"C:\" }));
+            Assert.AreEqual(@"C:\", IOUtils.FindRootPath(new []{ @"C:\Abc", @"C:\Def" }));
+            Assert.AreEqual(@"C:\", IOUtils.FindRootPath(new []{ @"C:", @"C:" }));
+            Assert.AreEqual(@"Test", IOUtils.FindRootPath(new []{ @"Test", @"Test" })); // no trailing slash
+
+            Assert.AreEqual(@"", IOUtils.FindRootPath(new []{ @"C:\Folder", @"D:\Folder", @"E:\Folder" }));
+            Assert.AreEqual(@"", IOUtils.FindRootPath(new []{ @"Invalid 1", @"Invalid 2" }));
+        }
+    }
+}
