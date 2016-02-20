2950aaa6536e10d65937b80699bab4dbb574a2d2
chylex <info@chylex.com>
2016-02-20 23:14:35 +0100

    Add JavaParseUtils with basic java code file processing using regex

---
+++ CodeStatistics/Handling/Languages/Java/JavaParseUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CodeStatistics.Handling.Languages.Java{
+    public static class JavaParseUtils{
+        private static readonly Regex RegexString = new Regex(@"([""'])(?:\\[\\'""btnfru0-7]|[^\\""])*?\1",RegexOptions.Compiled); // verbatim strings with quotes need "" for literal
+        private static readonly Regex RegexCommentSingle = new Regex(@"//.*?$",RegexOptions.Compiled | RegexOptions.Multiline);
+        private static readonly Regex RegexCommentMulti = new Regex(@"/\*.*?\*/",RegexOptions.Compiled | RegexOptions.Singleline);
+        
+        public static string ProcessCodeFile(string code){
+            string processed = code;
+            processed = RegexString.Replace(processed,@""""""); // beautiful
+            processed = RegexCommentSingle.Replace(processed,"");
+            processed = RegexCommentMulti.Replace(processed,"");
+            return processed;
+        }
+    }
+}
