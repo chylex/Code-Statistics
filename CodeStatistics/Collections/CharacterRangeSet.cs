599f644a8715ff8c7195bf86b8b2bdd2156ff199
chylex <info@chylex.com>
2016-02-21 23:00:10 +0100

    Add CharacterRangeSet with unit tests

---
+++ CodeStatistics/Collections/CharacterRangeSet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CodeStatistics.Collections{
+    public class CharacterRangeSet : IEnumerable<KeyValuePair<int,int>>{
+        private readonly List<KeyValuePair<int,int>> innerList = new List<KeyValuePair<int,int>>();
+
+        public void Add(int lowerBound, int upperBound){
+            innerList.Add(new KeyValuePair<int,int>(lowerBound,upperBound));
+        }
+
+        public bool Contains(int character){
+            foreach(KeyValuePair<int,int> kvp in innerList){
+                if (character < kvp.Key)return false; // all following KVPs are higher
+                if (character >= kvp.Key && character <= kvp.Value)return true;
+            }
+
+            return false;
+        }
+
+        public IEnumerator<KeyValuePair<int,int>> GetEnumerator(){
+            return innerList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator(){
+            return innerList.GetEnumerator();
+        }
+    }
+}
---
+++ CodeStatisticsTests/CollectionsTests.cs
@@ -0,0 +1,35 @@
+﻿using CodeStatistics.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeStatisticsTests{
+    [TestClass]
+    public class CollectionsTests{
+        [TestMethod]
+        public void TestCharacterRangeSet(){
+            CharacterRangeSet set = new CharacterRangeSet{
+                { 1, 10 },
+                { 16, 35 },
+                { 59, 59 },
+                { 61, 63 }
+            };
+
+            Assert.IsTrue(set.Contains(1));
+            Assert.IsTrue(set.Contains(2));
+            Assert.IsTrue(set.Contains(9));
+            Assert.IsTrue(set.Contains(10));
+            Assert.IsTrue(set.Contains(18));
+            Assert.IsTrue(set.Contains(24));
+            Assert.IsTrue(set.Contains(59));
+            Assert.IsTrue(set.Contains(61));
+            Assert.IsTrue(set.Contains(62));
+
+            Assert.IsFalse(set.Contains(0));
+            Assert.IsFalse(set.Contains(11));
+            Assert.IsFalse(set.Contains(13));
+            Assert.IsFalse(set.Contains(39));
+            Assert.IsFalse(set.Contains(58));
+            Assert.IsFalse(set.Contains(60));
+            Assert.IsFalse(set.Contains(100));
+        }
+    }
+}
