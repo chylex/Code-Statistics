a22252a0ed58bfe4d1bf1b9586678a5bcfdd2378
chylex <info@chylex.com>
2016-02-22 16:43:28 +0100

    Add Java element structs - Annotation & ImportStatement

---
+++ CodeStatistics/Handling/Languages/Java/Elements/Annotation.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeStatistics.Handling.Languages.Java.Elements{
+    public struct Annotation{
+        public readonly string SimpleName;
+
+        public Annotation(string simpleName){
+            this.SimpleName = simpleName;
+        }
+    }
+}
---
+++ CodeStatistics/Handling/Languages/Java/Elements/ImportStatement.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeStatistics.Handling.Languages.Java.Elements{
+    public struct ImportStatement{
+        public readonly string FullType;
+        public readonly bool IsStatic;
+
+        public ImportStatement(string fullType, bool isStatic = false){
+            this.FullType = fullType;
+            this.IsStatic = isStatic;
+        }
+    }
+}
