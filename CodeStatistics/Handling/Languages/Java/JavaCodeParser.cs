9502461ba2e5159cd28351debe3da7cf8d16e3f1
chylex <info@chylex.com>
2016-02-22 14:36:53 +0100

    Add a basic skeleton for JavaCodeParser class to handle java code files

---
+++ CodeStatistics/Handling/Languages/Java/JavaCodeParser.cs
@@ -0,0 +1,12 @@
+﻿using CodeStatistics.Handling.Utils;
+
+namespace CodeStatistics.Handling.Languages.Java{
+    class JavaCodeParser : CodeParser{
+        public JavaCodeParser(string code) : base(code){
+            IsWhiteSpace = JavaCharacters.IsWhiteSpace;
+            IsIdentifierStart = JavaCharacters.IsIdentifierStart;
+            IsIdentifierPart = JavaCharacters.IsIdentifierPart;
+            IsValidIdentifier = JavaCharacters.IsNotReservedWord;
+        }
+    }
+}
--- CodeStatistics/Handling/Languages/Java/JavaState.cs
+++ CodeStatistics/Handling/Languages/Java/JavaState.cs
@@ -1,21 +1,24 @@
-﻿using CodeStatistics.Input;
+﻿using CodeStatistics.Handling.Utils;
+using CodeStatistics.Input;
 using System.Collections.Generic;
 
 namespace CodeStatistics.Handling.Languages.Java{
     class JavaState{
         private readonly Dictionary<File,JavaFileInfo> fileInfo = new Dictionary<File,JavaFileInfo>();
 
+        public JavaFileInfo GetFile(File file){
+            return fileInfo[file];
+        }
+
         public JavaFileInfo Process(File file){
             JavaFileInfo info = new JavaFileInfo();
             fileInfo.Add(file,info);
 
+            JavaCodeParser parser = new JavaCodeParser(file.Contents);
+
             // TODO
 
             return info;
-        }
-
-        public JavaFileInfo GetFile(File file){
-            return fileInfo[file];
         }
     }
 }
