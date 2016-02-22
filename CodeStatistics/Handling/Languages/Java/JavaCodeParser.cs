6ca477d7dad6e79007be222685ed717058f4f076
chylex <info@chylex.com>
2016-02-22 16:51:20 +0100

    Add JavaCodeParser.ReadFullTypeName

--- CodeStatistics/Handling/Languages/Java/JavaCodeParser.cs
+++ CodeStatistics/Handling/Languages/Java/JavaCodeParser.cs
@@ -1,4 +1,6 @@
-﻿using CodeStatistics.Handling.Utils;
+﻿using CodeStatistics.Handling.Languages.Java.Elements;
+using CodeStatistics.Handling.Utils;
+using System.Text;
 
 namespace CodeStatistics.Handling.Languages.Java{
     class JavaCodeParser : CodeParser{
@@ -7,6 +9,31 @@ namespace CodeStatistics.Handling.Languages.Java{
             IsIdentifierStart = JavaCharacters.IsIdentifierStart;
             IsIdentifierPart = JavaCharacters.IsIdentifierPart;
             IsValidIdentifier = JavaCharacters.IsNotReservedWord;
+        }
+
+        /// <summary>
+        /// Reads the entire full type name, which consists of one or more identifiers separated by the dot character. <para/>
+        /// https://docs.oracle.com/javase/specs/jls/se8/html/jls-6.html#d5e7695
+        /// </summary>
+        public string ReadFullTypeName(){
+            StringBuilder build = new StringBuilder();
+
+            string identifier = NextIdentifier();
+            if (identifier.Length == 0)return string.Empty;
+
+            while(true){
+                build.Append(identifier);
+
+                if (Char == '.'){
+                    build.Append('.');
+
+                    identifier = Skip().NextIdentifier();
+                    if (identifier.Length == 0)return string.Empty;
+                }
+                else break;
+            }
+
+            return build.ToString();
         }
     }
 }
