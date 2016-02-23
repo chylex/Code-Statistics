8e5b416ab38c860b2e8a92977cf9521ed6cbe810
chylex <contact@chylex.com>
2016-02-23 20:27:06 +0100

    Add JavaParseUtils.ReadStructList for reading lists of elements in Java code

--- CodeStatistics/Handling/Languages/Java/Utils/JavaParseUtils.cs
+++ CodeStatistics/Handling/Languages/Java/Utils/JavaParseUtils.cs
@@ -1,10 +1,13 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace CodeStatistics.Handling.Languages.Java.Utils{
     public static class JavaParseUtils{
         private static readonly Regex RegexString = new Regex(@"([""'])(?:\\[\\'""btnfru0-7]|[^\\""])*?\1",RegexOptions.Compiled); // verbatim strings with quotes need "" for literal
         private static readonly Regex RegexCommentSingle = new Regex(@"//.*?$",RegexOptions.Compiled | RegexOptions.Multiline);
         private static readonly Regex RegexCommentMulti = new Regex(@"/\*.*?\*/",RegexOptions.Compiled | RegexOptions.Singleline);
+
+        public delegate T? ReadStruct<out T>() where T : struct;
         
         public static string PrepareCodeFile(string code){
             string processed = code;
@@ -17,6 +20,21 @@ namespace CodeStatistics.Handling.Languages.Java.Utils{
         public static string FullToSimpleName(string fullName){
             int lastDot = fullName.LastIndexOf('.');
             return lastDot == -1 ? fullName : lastDot < fullName.Length-1 ? fullName.Substring(lastDot+1) : string.Empty;
+        }
+
+        public static List<T> ReadStructList<T>(JavaCodeParser parser, ReadStruct<T> readFunc, int defaultCapacity) where T : struct{
+            var structs = new List<T>(defaultCapacity);
+
+            while(true){
+                parser.SkipSpaces();
+
+                T? structObj = readFunc();
+                
+                if (structObj.HasValue)structs.Add(structObj.Value);
+                else break;
+            }
+
+            return structs;
         }
     }
 }
