d709da2545b8deb754dfeee3bc98d65033fa9316
chylex <contact@chylex.com>
2016-02-23 16:11:34 +0100

    Override Equals, hash code, equality operators in Java Annotation struct

--- CodeStatistics/Handling/Languages/Java/Elements/Annotation.cs
+++ CodeStatistics/Handling/Languages/Java/Elements/Annotation.cs
@@ -1,9 +1,25 @@
 ﻿namespace CodeStatistics.Handling.Languages.Java.Elements{
     public struct Annotation{
+        public static bool operator ==(Annotation obj1, Annotation obj2){
+            return obj1.Equals(obj2);
+        }
+
+        public static bool operator !=(Annotation obj1, Annotation obj2){
+            return !obj1.Equals(obj2);
+        }
+
         public readonly string SimpleName;
 
         public Annotation(string simpleName){
             this.SimpleName = simpleName;
+        }
+
+        public override bool Equals(object obj){
+            return obj is Annotation && SimpleName == ((Annotation)obj).SimpleName;
+        }
+
+        public override int GetHashCode(){
+            return SimpleName.GetHashCode();
         }
     }
 }
