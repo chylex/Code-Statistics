660839eac80973a07eafe92f8e75dd08419f3fce
chylex <contact@chylex.com>
2016-02-24 19:38:38 +0100

    Add skeletons of Java Field and Method classes

---
+++ CodeStatistics/Handling/Languages/Java/Elements/Field.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeStatistics.Handling.Languages.Java.Elements{
+    public class Field : Member{
+        public readonly string Identifier;
+        public readonly Primitives? Type;
+
+        public Field(string identifier, Primitives? type, Member info) : base(info){
+            this.Identifier = identifier;
+            this.Type = type;
+        }
+    }
+}
---
+++ CodeStatistics/Handling/Languages/Java/Elements/Method.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CodeStatistics.Handling.Languages.Java.Elements{
+    public class Method : Member{
+        public readonly string Identifier;
+        public readonly Primitives? ReturnType;
+        public readonly ReadOnlyCollection<Primitives?> ParameterTypes;
+
+        public Method(string identifier, Primitives? returnType, List<Primitives?> parameterTypes, Member info) : base(info){
+            this.Identifier = identifier;
+            this.ReturnType = returnType;
+            this.ParameterTypes = parameterTypes.AsReadOnly();
+        }
+    }
+}
