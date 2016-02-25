cec14a9d8ea02b7bf8423889d41a5390d80542a4
chylex <contact@chylex.com>
2016-02-25 13:50:26 +0100

    Add TypeData class to Java Type for storing declaration type specific data

--- CodeStatistics/Handling/Languages/Java/Elements/Type.cs
+++ CodeStatistics/Handling/Languages/Java/Elements/Type.cs
@@ -10,9 +10,33 @@ namespace CodeStatistics.Handling.Languages.Java.Elements{
         public readonly string Identifier;
         public readonly List<Type> NestedTypes = new List<Type>(1);
 
+        private readonly TypeData data;
+
         public Type(DeclarationType type, string identifier, Member info) : base(info){
             this.Declaration = type;
             this.Identifier = identifier;
+
+            switch(type){
+                case DeclarationType.Enum: data = new DataEnum(); break;
+                default: data = new TypeData(); break;
+            }
+        }
+
+        public TypeData GetData(){
+            return data;
+        }
+
+        public T GetData<T>() where T : TypeData{
+            return data as T;
+        }
+
+        public class TypeData{
+            public readonly List<Method> Methods = new List<Method>(8);
+            public readonly List<Field> Fields = new List<Field>(8);
+        }
+
+        public class DataEnum : TypeData{
+            // TODO values
         }
     }
 }
