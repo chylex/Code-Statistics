02a4f7bd92b02a184c0693b5d322607a629aae35
chylex <info@chylex.com>
2016-02-27 15:23:42 +0100

    Provide Equals, GetHashCode, and equality operators in Java TypeOf

--- CodeStatistics/Handling/Languages/Java/Elements/TypeOf.cs
+++ CodeStatistics/Handling/Languages/Java/Elements/TypeOf.cs
@@ -22,6 +22,14 @@ namespace CodeStatistics.Handling.Languages.Java.Elements{
             return EmptyCollection;
         }
 
+        public static bool operator ==(TypeOf type1, TypeOf type2){
+            return type1.Equals(type2);
+        }
+
+        public static bool operator !=(TypeOf type1, TypeOf type2){
+            return !type1.Equals(type2);
+        }
+
         public bool IsVoid { get { return obj == null; } }
         public bool IsPrimitive { get { return obj is Primitives; } }
         public bool IsTypeObject { get { return !IsVoid && !IsPrimitive; } }
@@ -46,6 +54,18 @@ namespace CodeStatistics.Handling.Languages.Java.Elements{
             if (IsVoid)return "void";
             else if (IsPrimitive)return JavaPrimitives.ToString((Primitives)obj);
             else return obj.ToString();
+        }
+
+        public override int GetHashCode(){
+            return IsVoid ? 0 : IsPrimitive ? 1+(int)obj : obj.GetHashCode();
+        }
+
+        public override bool Equals(object otherObj){
+            if (otherObj is TypeOf){
+                TypeOf type = (TypeOf)otherObj;
+                return (IsVoid && type.IsVoid) || obj.Equals(type.obj);
+            }
+            else return false;
         }
     }
 }
