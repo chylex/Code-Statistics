466c7dfaa2d61e5fad0c809d3755854db1650d2f
chylex <info@chylex.com>
2016-02-28 13:33:44 +0100

    Add Java TypeIdentifier for easier identifier handling

---
+++ CodeStatistics/Handling/Languages/Java/Elements/TypeIdentifier.cs
@@ -0,0 +1,34 @@
+﻿namespace CodeStatistics.Handling.Languages.Java.Elements{
+    struct TypeIdentifier{
+        public static bool operator ==(TypeIdentifier obj1, TypeIdentifier obj2){
+            return obj1.Equals(obj2);
+        }
+
+        public static bool operator !=(TypeIdentifier obj1, TypeIdentifier obj2){
+            return !obj1.Equals(obj2);
+        }
+
+        public readonly string Prefix;
+        public readonly string Name;
+
+        public string Package { get { return Prefix.Length == 0 ? string.Empty : Prefix.Substring(0,Prefix.Length-1); } }
+        public string FullName { get { return Prefix+Name; } }
+
+        public TypeIdentifier(string prefix, string name){
+            this.Prefix = prefix.Length == 0 ? string.Empty : prefix+".";
+            this.Name = name;
+        }
+
+        public override string ToString(){
+            return FullName;
+        }
+
+        public override int GetHashCode(){
+            return FullName.GetHashCode();
+        }
+
+        public override bool Equals(object obj){
+            return obj is TypeIdentifier && FullName.Equals(((TypeIdentifier)obj).FullName);
+        }
+    }
+}
