234eb3e8a0677b819467f20a405390291f291745
chylex <info@chylex.com>
2016-02-29 13:52:32 +0100

    Fix Java string characters inside comments

--- CodeStatistics/Handling/Languages/Java/Utils/JavaParseUtils.cs
+++ CodeStatistics/Handling/Languages/Java/Utils/JavaParseUtils.cs
@@ -3,7 +3,8 @@ using System.Text.RegularExpressions;
 
 namespace CodeStatistics.Handling.Languages.Java.Utils{
     public static class JavaParseUtils{
-        private static readonly Regex RegexString = new Regex(@"([""'])(?:\\[\\'""btnfru0-7]|[^\\""])*?\1",RegexOptions.Compiled); // verbatim strings with quotes need "" for literal
+        // verbatim strings with quotes need "" for literal
+        private static readonly Regex RegexString = new Regex(@"([""'])(?:\\[\\'""btnfru0-7]|[^\\""])*?(?:\1|$|(?=\*/))",RegexOptions.Compiled | RegexOptions.Multiline);
         private static readonly Regex RegexCommentSingle = new Regex(@"//.*?$",RegexOptions.Compiled | RegexOptions.Multiline);
         private static readonly Regex RegexCommentMulti = new Regex(@"/\*.*?\*/",RegexOptions.Compiled | RegexOptions.Singleline);
 
