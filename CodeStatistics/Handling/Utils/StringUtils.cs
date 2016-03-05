08fc37be1c4484786b1f83b88ac6b0d537ba6c6b
chylex <info@chylex.com>
2016-03-05 17:11:48 +0100

    Add StringUtils.CapitalizeFirst with unit tests

--- CodeStatistics/Handling/Utils/StringUtils.cs
+++ CodeStatistics/Handling/Utils/StringUtils.cs
@@ -1,4 +1,10 @@
 ﻿namespace CodeStatistics.Handling.Utils{
     public static class StringUtils{
+        /// <summary>
+        /// Capitalizes the first character of a string and leaves the rest intact.
+        /// </summary>
+        public static string CapitalizeFirst(this string me){
+            return me.Length == 0 ? string.Empty : me.Length == 1 ? me.ToUpperInvariant() : me.Substring(0,1).ToUpperInvariant()+me.Substring(1);
+        }
     }
 }
--- CodeStatisticsTests/StringUtilsTests.cs
+++ CodeStatisticsTests/StringUtilsTests.cs
@@ -4,6 +4,15 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CodeStatisticsTests{
     [TestClass]
     public class StringUtilsTests{
-        
+        [TestMethod]
+        public void TestCapitalizeFirst(){
+            Assert.AreEqual("", "".CapitalizeFirst());
+            Assert.AreEqual(" ", " ".CapitalizeFirst());
+            Assert.AreEqual("X", "x".CapitalizeFirst());
+            Assert.AreEqual("X", "X".CapitalizeFirst());
+            Assert.AreEqual("Hello", "hello".CapitalizeFirst());
+            Assert.AreEqual("HELLO", "hELLO".CapitalizeFirst());
+            Assert.AreEqual("HELLO", "HELLO".CapitalizeFirst());
+        }
     }
 }
