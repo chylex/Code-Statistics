f6e07f3247bb45aa675272b21008e6a4af6e31e5
chylex <info@chylex.com>
2016-03-05 17:10:13 +0100

    Remove all unused methods from StringUtils

--- CodeStatisticsTests/StringUtilsTests.cs
+++ CodeStatisticsTests/StringUtilsTests.cs
@@ -4,43 +4,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CodeStatisticsTests{
     [TestClass]
     public class StringUtilsTests{
-        [TestMethod]
-        public void TestStringExtract(){
-            Assert.AreEqual("do not touch letters","...do not touch letters".ExtractStart("..."));
-            Assert.AreEqual("..do not touch letters","..do not touch letters".ExtractStart("..."));
-            Assert.AreEqual("do not touch letters","do not touch letters...".ExtractEnd("..."));
-            Assert.AreEqual("do not touch letters..","do not touch letters..".ExtractEnd("..."));
-            Assert.AreEqual("do not touch letters","...do not touch letters___".ExtractBoth("...","___"));
-            Assert.AreEqual("do not touch letters__","...do not touch letters__".ExtractBoth("...","___"));
-            Assert.AreEqual("..do not touch letters","..do not touch letters___".ExtractBoth("...","___"));
-            Assert.AreEqual("..do not touch letters__","..do not touch letters__".ExtractBoth("...","___"));
-        }
-
-        [TestMethod]
-        public void TestStringExtractOut(){
-            string res;
-
-            Assert.IsTrue("...do not touch letters".ExtractStart("...",out res));
-            Assert.AreEqual("do not touch letters",res);
-            Assert.IsFalse("..do not touch letters".ExtractStart("...",out res));
-
-            Assert.IsTrue("do not touch letters...".ExtractEnd("...",out res));
-            Assert.AreEqual("do not touch letters",res);
-            Assert.IsFalse("do not touch letters..".ExtractEnd("...",out res));
-
-            Assert.IsTrue("...do not touch letters___".ExtractBoth("...","___",out res));
-            Assert.AreEqual("do not touch letters",res);
-            Assert.IsFalse("___do not touch letters...".ExtractBoth("...","___",out res));
-            Assert.IsFalse("...do not touch letters".ExtractBoth("...","___",out res));
-            Assert.IsFalse("do not touch letters___".ExtractBoth("...","___",out res));
-        }
-
-        [TestMethod]
-        public void TestStringRemove(){
-            Assert.AreEqual("this is the end","this is the end, or not".RemoveFrom(","));
-            Assert.AreEqual("this is the end","this is the end".RemoveFrom(","));
-            Assert.AreEqual("this is the end","no way, this is the end".RemoveTo(", "));
-            Assert.AreEqual("this is the end","this is the end".RemoveTo(", "));
-        }
+        
     }
 }
