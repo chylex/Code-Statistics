cc2ff88504434fa6e37552d6f59c09ecb33e76a8
chylex <info@chylex.com>
2016-03-06 03:03:07 +0100

    Add CounterDictionary

---
+++ CodeStatistics/Collections/CounterDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeStatistics.Collections{
+    class CounterDictionary<TKey> : IEnumerable<KeyValuePair<TKey,int>>{
+        private readonly Dictionary<TKey,int> innerDict;
+
+        public int Count { get { return innerDict.Count; } }
+
+        public CounterDictionary(){
+            innerDict = new Dictionary<TKey,int>();
+        }
+
+        public CounterDictionary(int capacity){
+            innerDict = new Dictionary<TKey,int>(capacity);
+        }
+
+        public void Increment(TKey key){
+            if (innerDict.ContainsKey(key)){
+                ++innerDict[key];
+            }
+            else{
+                innerDict[key] = 1;
+            }
+        }
+
+        public List<KeyValuePair<TKey,int>> ListFromTop(){
+            List<KeyValuePair<TKey,int>> list = innerDict.ToList();
+            list.Sort((kvp1, kvp2) => kvp2.Value-kvp1.Value);
+            return list;
+        }
+
+        public IEnumerator<KeyValuePair<TKey,int>> GetEnumerator(){
+            return innerDict.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator(){
+            return innerDict.GetEnumerator();
+        }
+    }
+}
