3e30a964db4e28f4a699e0faec87b8ce5233ae8d
chylex <info@chylex.com>
2016-03-10 13:13:08 +0100

    Add a static EnumDictionary class to simplify dicts with Enum keys

---
+++ CodeStatistics/Collections/EnumDictionary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeStatistics.Collections{
+    public static class EnumDictionary{
+        /// <summary>
+        /// Creates a Dictionary&lt;Enum,TValue&gt; and prefills it with <paramref name="initializeTo"/> assigned to each element of the Enum.
+        /// </summary>
+        public static Dictionary<TKey,TValue> Create<TKey,TValue>(TValue initializeTo = default(TValue)) where TKey : struct{
+            Array values = Enum.GetValues(typeof(TKey));
+            Dictionary<TKey,TValue> dict = new Dictionary<TKey,TValue>(values.Length);
+
+            foreach(object value in values){
+                dict[(TKey)value] = initializeTo;
+            }
+
+            return dict;
+        }
+    }
+}
