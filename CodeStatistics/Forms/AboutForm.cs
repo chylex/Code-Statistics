0b80d3e04abe8b497b32b8010bb22ea5b60e86e1
chylex <info@chylex.com>
2016-03-13 12:54:17 +0100

    Refactor Mono specific code in AboutForm and fix broken button anchor

--- CodeStatistics/Forms/AboutForm.cs
+++ CodeStatistics/Forms/AboutForm.cs
@@ -19,22 +19,28 @@ namespace CodeStatistics.Forms{
 
         public AboutForm(){
             InitializeComponent();
-#if !MONO
-            this.HelpButton = true; // Mono does not support OnHelpButtonClicked
-            this.HelpButtonClicked += OnHelpButtonClicked;
-#endif
+
             Text = Lang.Get["TitleAbout"];
             btnReadme.Text = Lang.Get["AboutReadme"];
             textContents.Rtf = AboutFormData.Aggregate(Resources.about,(contents, kvp) => contents.Replace(kvp.Key,kvp.Value));
+
+#if !MONO
+            HelpButton = true; // Mono does not support OnHelpButtonClicked
+            HelpButtonClicked += OnHelpButtonClicked;
+#else
+            btnReadme.Location = new System.Drawing.Point(292-(btnReadme.Size.Width-50),btnReadme.Location.Y); // it does however fuck up the button anchor quite well
+#endif
         }
+
+        private void btnReadme_Click(object sender, EventArgs e){
+            Process.Start("https://github.com/chylex/Code-Statistics/blob/master/README.md");
+        }
+
 #if !MONO
         private void OnHelpButtonClicked(object sender, System.ComponentModel.CancelEventArgs e){
             e.Cancel = true;
             Process.Start("https://github.com/chylex/Code-Statistics/issues");
         }
 #endif
-        private void btnReadme_Click(object sender, EventArgs e){
-            Process.Start("https://github.com/chylex/Code-Statistics/blob/master/README.md");
-        }
     }
 }
