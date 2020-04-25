b46c6e602c6336d8e5f63aef5c1d4c4aca36f281
chylex <contact@chylex.com>
2020-04-25 20:29:12 +0200

    Hello World

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CodeStatistics{
+    static class Program{
+        private static void Main(string[] args){
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
